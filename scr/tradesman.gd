414444074c957887cb4e105e2c11e764176836e7
JorsonV2 <38647517+JorsonV2@users.noreply.github.com>
2020-04-03 12:01:22 +0200

    add basic map scene

---
+++ scr/tradesman.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+export var cost_for_gold = [0,0,0]
+export var cost_for_wood = [0,0,0]
+export var cost_for_rock = [0,0,0]
+export var cost_for_leather = [0,0,0]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
