a3b78f2db0bfe260b81fc45f86bda9210cbea3bf
JorsonV2 <38647517+JorsonV2@users.noreply.github.com>
2020-04-04 00:29:44 +0200

    fix trigger setting player_destination

--- scr/next_map_trigger.gd
+++ scr/next_map_trigger.gd
@@ -14,6 +14,8 @@ func _ready():
 func _on_next_map_trigger_area_entered(area):
 	if area.is_in_group("player"):
 		var new_map
+		var old_map
+		
 		match map_name:
 			"village":
 				new_map = game_controller.village_scene.instance()
@@ -21,13 +23,21 @@ func _on_next_map_trigger_area_entered(area):
 				new_map = game_controller.forest_scene.instance()
 			"deep_forest":
 				new_map = game_controller.deep_forest_scene.instance()
-		var old_map = get_tree().get_root().get_node("map")
+				
+		if trigger_side == "left":
+			game_controller.player_destination = "right"
+		else:
+			game_controller.player_destination = "left"
+				
+		old_map = get_tree().get_root().get_node("map")
 		old_map.set_name("map1")
 		
 		game_controller.scene_changer.get_node("Control").show()
 		animation_map = game_controller.scene_changer.find_node("animation_map")
 		black = game_controller.scene_changer.find_node("panel_black")
+		
 		#yield(get_tree().create_timer(time_delay), "timeout")
+		
 		game_controller.player.active_move=false
 		animation_map.play("fade")
 		yield(animation_map,"animation_finished")
@@ -41,12 +51,6 @@ func _on_next_map_trigger_area_entered(area):
 		animation_map.play_backwards("fade")
 		yield(animation_map,"animation_finished")
 		old_map.queue_free()
-		
-		
-		if trigger_side == "left":
-			game_controller.player_destination = "right"
-		else:
-			game_controller.player_destination = "left"
 		
 		
 		
